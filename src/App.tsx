9dadf5941ff903a64adb6bc11ef3aef5e6887b5d
matheusifz <matheusifz@gmail.com>
2021-10-29 19:30:49 -0300

    feat: create navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from "react";
+import Navbar from "./components/navbar/";
 
 function App() {
   return (
     <div className="App">
-      <div className="header">
-        <div className="Nav">
-          <h3>Home</h3>
-          <h3>About</h3>
-          <h3>Contact</h3>
-        </div>
-      </div>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar/styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Header = styled.h3`
+  color: red;
+`;
