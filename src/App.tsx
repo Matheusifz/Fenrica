89a9afd7ae736a1cddd56b0238a335ada7f77462
matheusifz <matheusifz@gmail.com>
2021-10-29 18:13:28 -0300

    feat: create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="header">
+        <div className="Nav">
+          <h3>Home</h3>
+          <h3>About</h3>
+          <h3>Contact</h3>
+        </div>
+      </div>
     </div>
   );
 }
