5c7299cd30c023a968af5a9e7e6adeca85e326d0
matheusifz <matheusifz@gmail.com>
2021-11-01 19:41:13 -0300

    feat: navbar style

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-import { Header } from "./styles";
+import { NavItems, Header } from "./styles";
 
 interface Navbar {}
 
 const Navbar: React.FC = () => {
   return (
-    <div className="header">
-      <div className="Nav">
-        <h3>Home</h3>
-        <h3>About</h3>
-        <h3>Contact</h3>
-      </div>
-    </div>
+    <Header>
+      <NavItems>Home</NavItems>
+      <NavItems>Services</NavItems>
+      <NavItems>Contact</NavItems>
+    </Header>
   );
 };
-
 export default Navbar;
--- src/components/navbar/styles.ts
+++ src/components/navbar/styles.ts
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 
-export const Header = styled.h3`
-  color: red;
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-around;
+  background-color: #363534;
+`;
+
+export const NavItems = styled.h3`
+  display: flex;
+  box-sizing: border-box;
+  margin: 0;
+  margin-top: 15px;
+  padding: 0;
+  font-family: "Lato", sans-serif;
+  color: #f0f0f0;
 `;
