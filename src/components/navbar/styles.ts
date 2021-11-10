f087da3f4a561d721f323e16e300a39b8bef4e56
matheusifz <matheusifz@gmail.com>
2021-11-10 16:30:52 -0300

    feat: change background and navbar color

--- src/components/navbar/styles.ts
+++ src/components/navbar/styles.ts
@@ -3,14 +3,28 @@ import styled from "styled-components";
 export const Header = styled.div`
   display: flex;
   justify-content: space-around;
+  padding: 20px;
 `;
 
 export const NavItems = styled.h3`
   display: flex;
   box-sizing: border-box;
   margin: 0;
-  margin-top: 15px;
+  margin-top: 10px;
   padding: 0;
   font-family: "Lato", sans-serif;
-  color: #f0f0f0;
+  color: #72c0aa;
+  font-weight: 300;
+`;
+
+export const Contact = styled.h3`
+  display: inline-block;
+  width: 150px;
+  padding: 8px;
+  color: #306457;
+  border: 1px solid #306457;
+  text-align: center;
+  font-family: "Lato", sans-serif;
+  font-weight: 400;
+  border-radius: 25px;
 `;
